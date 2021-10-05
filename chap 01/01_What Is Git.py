20fc46f76d4c3cdde57970a7690c9f4908eddf9e
MohammedHassan25 <87442047+MohammedHassan25@users.noreply.github.com>
2021-10-05 23:14:41 +0200

    chap 01 : What Is Git ?
    chap 01 : What Is Git ?

---
+++ chap 01/01_What Is Git.py
@@ -0,0 +1,5 @@
+# -------------------------------------------------------------------------------------------------------------------------------------------------------
+# Git is software that keeps track of changes that you make to files and directories. And it's especially good at keeping track of text changes.
+# Git is referred to as a version control system or VCS for short.
+# It's also open-source and free
+# -------------------------------------------------------------------------------------------------------------------------------------------------------
